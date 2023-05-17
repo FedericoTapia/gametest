f18575fc385b3089009ee630d538d64fd517d731
Federico <federicotapia080501@gmail.com>
2023-05-17 13:39:31 -0300

    actualizacion de fondo

--- tpjuego/js/background.js
+++ tpjuego/js/background.js
@@ -1,14 +1,15 @@
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 10;
 
-const backgroundLayer1 = new Image();
+const backgroundLayer1 = new Image(); //cielo
 backgroundLayer1.src = 'image/layer-1.png';
-const backgroundLayer2 = new Image();
+const backgroundLayer2 = new Image(); 
 backgroundLayer2.src = 'image/layer-2.png';
-const backgroundLayer3 = new Image();
+const backgroundLayer3 = new Image(); 
 backgroundLayer3.src = 'image/layer-3.png';
 const backgroundLayer4 = new Image();
 backgroundLayer4.src = 'image/layer-4.png';
@@ -45,8 +46,8 @@ class Layer {
     }
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer1 = new Layer(backgroundLayer1, 0.1);
+const layer2 = new Layer(backgroundLayer2, 0.2);
 const layer3 = new Layer(backgroundLayer3, 0.6);
 const layer4 = new Layer(backgroundLayer4, 0.8);
 const layer5 = new Layer(backgroundLayer5, 1);
